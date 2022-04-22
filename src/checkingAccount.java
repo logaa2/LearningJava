b2087a649906ee1a192ad8b6054ee0490b32193c
Leonardo <cfl161122@gmail.com>
2022-04-22 16:12:49 -0700

    thirdtimesthecharm

---
+++ src/checkingAccount.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class checkingAccount {
+    float balance;
+
+    public float withdrawal() {
+        System.out.print("Withdrawal ammount: ");
+        Scanner scanner = new Scanner(System.in);
+        float input = scanner.nextFloat();
+        return input;
+    }
+}
---
+++ src/person.java
@@ -0,0 +1,7 @@
+public class person {
+    protected String firstName;
+    protected String middleName;
+    protected String lastName;
+    protected int age;
+    protected String socialSecurityNumber;
+}
