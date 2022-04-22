53fd9f74163a5a895074b83a2baa0ae9ade083e1
Leonardo <cfl161122@gmail.com>
2022-04-22 15:53:30 -0700

    newFirst

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+/*b) Bank Management System:
+        * Create a Bank Management System which has the following components:
+        * Person (which saves first, middle and last name, age and a "social security number")
+        * Bank (which saves a List of customers and their Checking Accounts, maximum overdraw amount)
+        * Checking Account (should have a current value, methods for taking and depositing money)
+        * Opening a Checking Account should only work via a Bank and must require a certain amount of initial deposit.
+        *
+        * BONUS: Make the Banking System Interactive (No Solution provided!)*/
+public class main{
+    public static void Main()
+    {
+        System.out.println("Welcome to Leo Banking :)");
+        System.out.println("Are you a? ");
+        System.out.println("1 ) Bank Member");
+        System.out.println("2 ) New Customer");
+    }   
+}
